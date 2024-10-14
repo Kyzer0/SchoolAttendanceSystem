c73e47797f916266e6f67d7b467391ab5839e2bc
Jervie Gono <112779985+Kyzer0@users.noreply.github.com>
2024-10-14 22:14:59 +0800

    Add files via upload

---
+++ FlaskWebProject1/main/script.js
@@ -0,0 +1,8 @@
+
+const btn = document.getElementById('class-section')
+const choices = document.getElementById('choices')
+btn.addEventListener('click', (e) => {
+
+    choices.classList.toggle('hide')
+
+})
